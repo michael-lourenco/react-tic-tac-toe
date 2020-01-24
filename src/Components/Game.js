ed5b3a43b756197d275cc5345bacc71c360cec40
Michael Lourenco <kontempler@gmail.com>
2020-01-24 01:05:04 -0300

    enviando base do app tic tac toe

---
+++ src/Components/Game.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import Board from './Board'
+// import { Container } from './styles';
+
+export default class Game extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      xIsNext:true,
+      stepNumber:0,
+      history:[
+        {squares:Array(9).fill(null)}
+      ]
+    }
+  }
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber]
+
+    return (
+    <div className="game">
+      <div className="game-board">
+        <Board onClick={(i)=>this.onClick(i)}
+        squares={current.squares}
+        />
+      </div>
+    </div>)
+  }
+}
